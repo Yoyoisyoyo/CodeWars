3aff730823200ab9eab34b78d52ac98f9ca77ff6
Jingyi You <jingyiyou@Jingyis-MacBook-Air.local>
2022-02-24 21:18:55 -0800

    create pirate problem

---
+++ CodeWars_Problems/CodeWars_Problems/Pirates8.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CodeWars_Problems
+{
+	public static class Pirates8
+	{
+		public static string CannonsReady(Dictionary<string, string> gunners)
+		{
+			string order = "Fire!";
+			return order;
+		}
+	}
+}
+
---
+++ CodeWars_Problems/CodeWars_Problems/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CodeWars_Problems;
+
+Dictionary<string, string> gunners = new Dictionary<string, string>()
+{
+
+};
+
+string order = Pirates8.CannonsReady(gunners);
+Console.WriteLine(order);
